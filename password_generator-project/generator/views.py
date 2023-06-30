d1e052c4a4fbf69461124824432bf42f3dd7edb9
Tedis Rozenfelds <trozenfelds@gocardless.com>
2023-06-30 09:12:38 +0300

    Using Form Data for generator

--- password_generator-project/generator/views.py
+++ password_generator-project/generator/views.py
@@ -13,7 +13,20 @@ def password(request):
     # List individual characters into list
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
-    length = 10
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@£$%^&*()'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('123456789'))
+    # Get value of key length in url
+    # You can hard code the value to 100 example and it would
+    # generate password with 100 characters - it can be handled by
+    # adding if statements
+    # 12 sets the default value
+    length = int(request.GET.get('length', 12))
 
     thepassword = ''
     for x in range(length):
