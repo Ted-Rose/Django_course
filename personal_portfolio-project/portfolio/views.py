905f8499054ac42395c0d2137e34f3bcb05efb6f
Tedis Rozenfelds <trozenfelds@gocardless.com>
2023-10-13 23:13:00 +0300

    Displaying Objects part 2

--- personal_portfolio-project/portfolio/views.py
+++ personal_portfolio-project/portfolio/views.py
@@ -4,4 +4,5 @@ from .models import Project
 def home(request):
     # Grab all db objects
     projects = Project.objects.all()
+    # 'projects' is the name of the variable that can be named differently
     return render(request, 'portfolio/home.html', {'projects':projects})
