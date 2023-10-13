8a51eeb0fa50d1e5f2314af775c7386479381c66
Tedis Rozenfelds <trozenfelds@gocardless.com>
2023-10-13 23:53:51 +0300

    5 - 7 Blog Model

--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    # Grab latest 5 db objects ordered by date descending
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
