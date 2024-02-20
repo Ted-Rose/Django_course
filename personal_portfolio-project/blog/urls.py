cdccda2defbc914705ff91d0f0051ff607abe881
Tedis Rozenfelds <trozenfelds@gocardless.com>
2024-02-20 11:39:15 +0200

    5 - 10 Detail Page

--- personal_portfolio-project/blog/urls.py
+++ personal_portfolio-project/blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 # From current directory import views
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
+    # When /blog/int is accessed, views.detail will be called
+    path('<int:blog_id>', views.detail, name='detail'),
 ]
